1e04232513ca3c7ee20102463f5f3a631c83318c
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-08 00:57:33 +0900

    feat: 직접 구현한 hooks

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,10 +1,16 @@
-/* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import { DependencyList } from "react";
+import { useMemo } from "./useMemo";
 
+// useCallback: 의존성이 변경되면 함수 실행
+// 1. useMemo에서 의존성, 함수 비교
+// 2. useMemo에서 함수 반환
 export function useCallback<T extends Function>(
   factory: T,
   _deps: DependencyList,
 ) {
-  // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  const memorizedFunc = useMemo(() => factory, _deps);
+
+  return memorizedFunc;
 }
--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,12 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
 export function useMemo<T>(
   factory: () => T,
   _deps: DependencyList,
   _equals = shallowEquals,
 ): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  // 1. 이전 의존성과 결과를 저장할 ref 생성
+  const ref = useRef<{ deps: DependencyList; result: T } | null>(null);
+
+  // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
+  if (ref.current === null) {
+    ref.current = { deps: _deps, result: factory() };
+  }
+
+  // 2. 현재 의존성과 이전 의존성 비교
+  if (!_equals(ref.current.deps, _deps)) {
+    ref.current = { deps: _deps, result: factory() };
+  }
+
+  return ref.current.result;
 }
