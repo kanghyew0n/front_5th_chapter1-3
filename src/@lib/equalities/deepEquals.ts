00518bf7a4a4e72512a369060a6284b6e5e08206
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-07 23:04:36 +0900

    feat: 비교함수 구현하기

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,27 @@
+const isObject = (obj: unknown): obj is Record<string, unknown> => {
+  return typeof obj === "object" && obj !== null;
+};
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB || (objA === null && objB === null)) {
+    return true;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+    return objA.every((value, key) => deepEquals(value, objB[key]));
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const entriesA = Object.entries(objA);
+    const entriesB = Object.entries(objB);
+    return (
+      entriesA.length === entriesB.length &&
+      entriesA.every(([key, value]) => deepEquals(value, objB[key]))
+    );
+  }
+
+  return false;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,27 @@
+const isObject = (obj: unknown): obj is Record<string, unknown> => {
+  return typeof obj === "object" && obj !== null;
+};
+
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+    return objA.every((value, key) => objB[key] === value);
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const entriesA = Object.entries(objA);
+    const entriesB = Object.entries(objB);
+    return (
+      entriesA.length === entriesB.length &&
+      entriesA.every(([key, value]) => objB[key] === value)
+    );
+  }
+
+  return false;
 }
