97d4b2090f1315e46aae1eefecb58b1b18935d73
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-10 23:38:04 +0900

    chore: 배포

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 
 export default mergeConfig(
   defineConfig({
+    base:
+      process.env.NODE_ENV === "production" ? "/front_5th_chapter1-3/" : "/",
+    build: {
+      outDir: "dist",
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          404: resolve(__dirname, "404.html"),
+        },
+      },
+    },
     plugins: [react()],
   }),
   defineTestConfig({
